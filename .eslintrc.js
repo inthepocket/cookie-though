56609bb4ed494ea44122c65d5905069cf4436c50
Lukas Cornille <lukas.cornille@inthepocket.mobi>
2021-03-01 09:28:36 +0100

    Update dependency eslint-config-prettier to v8

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,10 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
-    'prettier/@typescript-eslint',
-    'prettier/react',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
